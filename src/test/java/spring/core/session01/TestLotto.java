f1fca9e24c2ec0cdca34c75e7e32ea2a9bab9eb3
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-30 21:16:06 +0800

    spring

---
+++ src/test/java/spring/core/session01/TestLotto.java
@@ -0,0 +1,13 @@
+package spring.core.session01;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestLotto {
+	@Test
+	public void test() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+	}
+}
