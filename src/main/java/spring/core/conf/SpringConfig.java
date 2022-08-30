c7f75c1006872cc0dc597b0a50fed730153f1501
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-30 21:39:11 +0800

    spring

--- src/main/java/spring/core/conf/SpringConfig.java
+++ src/main/java/spring/core/conf/SpringConfig.java
@@ -1,9 +1,14 @@
 package spring.core.conf;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import spring.core.session01.beans.Hello;
+import spring.core.session01.beans.Lotto;
+import spring.core.session01.beans.Lucky;
 
 // Spring Java 配置, 可以用來取代 conf/applicationContext.xml 配置
 // 宣告此類是一個配置定義
@@ -12,9 +17,25 @@ public class SpringConfig {
 	
 	// <bean id="hello" class="spring.core.session01.beans.Hello" />
 	@Bean(name = "hello")
+	@Scope(SCOPE_SINGLETON) // 預設就是 SCOPE_SINGLETON
 	public Hello getHello() {
 		Hello hello = new Hello();
 		return hello;
 	}
 	
+	@Bean(name = "lotto")
+	public Lotto getLotto() {
+		return new Lotto();
+	}
+	
+	@Bean(name = "lotto2")
+	public Lotto getLotto2() {
+		return new Lotto();
+	}
+	
+	@Bean(name = "lucky")
+	@Scope(SCOPE_PROTOTYPE)
+	public Lucky getLucky() {
+		return new Lucky();
+	}
 }
--- src/test/java/spring/core/session01/TestLucky.java
+++ src/test/java/spring/core/session01/TestLucky.java
@@ -4,10 +4,15 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.core.session01.beans.Lucky;
+
 public class TestLucky {
 	@Test
 	public void test() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		for(int i=0;i<10;i++) {
+			Lucky lucky = ctx.getBean(Lucky.class);
+			System.out.println(lucky);
+		}
 	}
 }
