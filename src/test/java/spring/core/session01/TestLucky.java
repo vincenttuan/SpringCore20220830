40ceaf44d2ee49a861eb0c8349b9a766e70b6698
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-30 21:40:35 +0800

    spring

--- src/test/java/spring/core/session01/TestLucky.java
+++ src/test/java/spring/core/session01/TestLucky.java
@@ -2,14 +2,17 @@ package spring.core.session01;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.core.conf.SpringConfig;
 import spring.core.session01.beans.Lucky;
 
 public class TestLucky {
 	@Test
 	public void test() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
 		for(int i=0;i<10;i++) {
 			Lucky lucky = ctx.getBean(Lucky.class);
 			System.out.println(lucky);
