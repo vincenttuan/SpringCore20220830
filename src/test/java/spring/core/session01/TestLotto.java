09fdc01e1626e0903dfff9a8da3fe026f031860a
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-30 21:24:25 +0800

    spring

--- src/test/java/spring/core/session01/TestLotto.java
+++ src/test/java/spring/core/session01/TestLotto.java
@@ -4,10 +4,15 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.core.session01.beans.Lotto;
+
 public class TestLotto {
 	@Test
 	public void test() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		Lotto lotto1 = ctx.getBean("lotto", Lotto.class);
+		Lotto lotto2 = ctx.getBean("lotto2", Lotto.class);
+		System.out.println(lotto1);
+		System.out.println(lotto2);
 	}
 }
