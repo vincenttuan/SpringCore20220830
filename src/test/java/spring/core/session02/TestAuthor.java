3210025dd2ca68584833eb246b942b45c906f0b2
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-01 19:16:57 +0800

    spring

--- src/test/java/spring/core/session02/TestAuthor.java
+++ src/test/java/spring/core/session02/TestAuthor.java
@@ -21,5 +21,17 @@ public class TestAuthor {
 		Author author2 = ctx.getBean("author2", Author.class);
 		// author2 在配置檔中已經有預設注入資料(利用方法注入)
 		System.out.println(author2);
+		
+		Author author3 = ctx.getBean("author3", Author.class);
+		// author3 在配置檔中已經有預設注入資料(利用建構子注入)
+		System.out.println(author3);
+		
+		Author author4 = ctx.getBean("author4", Author.class);
+		// author4 在配置檔中已經有預設注入資料(利用 p 方法注入)
+		System.out.println(author4);
+		
+		Author author5 = ctx.getBean("author5", Author.class);
+		// author5 在配置檔中已經有預設注入資料(利用 c 建構子注入)
+		System.out.println(author5);	
 	}
 }
