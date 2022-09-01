03e015e4abe193db41288b77e4fca2cb61e6c5c8
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-01 21:18:00 +0800

    spring

--- src/test/java/spring/core/session02/TestPaper.java
+++ src/test/java/spring/core/session02/TestPaper.java
@@ -10,8 +10,11 @@ public class TestPaper {
 	@Test
 	public void test() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Paper paper = ctx.getBean(Paper.class);
+		Paper paper = ctx.getBean("paper", Paper.class);
 		System.out.println(paper);
+		
+		Paper paper2 = ctx.getBean("paper2", Paper.class);
+		System.out.println(paper2);
 		
 	}
 }
