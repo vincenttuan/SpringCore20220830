ffcc5dc94034d2008286d8c7c53272a6e824337f
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-01 21:05:59 +0800

    spring

--- src/main/java/spring/core/session02/beans/material/Size.java
+++ src/main/java/spring/core/session02/beans/material/Size.java
@@ -1,5 +1,21 @@
 package spring.core.session02.beans.material;
 
+// 尺寸
 public class Size {
+	
+	private String name; // 尺寸型號
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Size [name=" + name + "]";
+	}
+	
 }
