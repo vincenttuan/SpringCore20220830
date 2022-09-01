f58df51e71faea134d6d97d599283d82c258c893
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-01 19:23:03 +0800

    spring

--- src/main/java/spring/core/conf/SpringConfig.java
+++ src/main/java/spring/core/conf/SpringConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 import spring.core.session01.beans.Hello;
 import spring.core.session01.beans.Lotto;
 import spring.core.session01.beans.Lucky;
+import spring.core.session02.beans.Author;
 
 // Spring Java 配置, 可以用來取代 conf/applicationContext.xml 配置
 // 宣告此類是一個配置定義
@@ -38,4 +39,11 @@ public class SpringConfig {
 	public Lucky getLucky() {
 		return new Lucky();
 	}
+	
+	@Bean(name = "author")
+	public Author getAuthor() {
+		Author author = new Author("Vincent", 'M', 30);
+		return author;
+	}
+	
 }
