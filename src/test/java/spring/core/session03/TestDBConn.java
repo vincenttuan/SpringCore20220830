2225c989bc7e4efcc42b2989c6594d26534b33ed
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-06 21:44:18 +0800

    spring

--- src/test/java/spring/core/session03/TestDBConn.java
+++ src/test/java/spring/core/session03/TestDBConn.java
@@ -2,6 +2,7 @@ package spring.core.session03;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.core.session03.beans.DBConn;
@@ -10,11 +11,14 @@ public class TestDBConn {
 	@Test
 	public void test() {
 		
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		DBConn dbConn = ctx.getBean("dbConn", DBConn.class);
 		dbConn.find();
 		dbConn.find();
 		dbConn.find();
-		
+		// 關閉 close() 方法在 AbstractApplicationContext 類中有定義
+		ctx.close();
 	}
 }
