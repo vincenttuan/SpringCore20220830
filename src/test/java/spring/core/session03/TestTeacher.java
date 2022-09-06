8f5431129f6a2bc8298ba7e1ddfdf8d990f24872
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-06 19:53:54 +0800

    spring

---
+++ src/test/java/spring/core/session03/TestTeacher.java
@@ -0,0 +1,16 @@
+package spring.core.session03;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session03.beans.Teacher;
+
+public class TestTeacher {
+	@Test
+	public void test() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Teacher teacher1 = ctx.getBean("teacher1", Teacher.class);
+		System.out.println(teacher1);
+	}
+}
