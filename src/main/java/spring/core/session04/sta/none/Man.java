22cc76ce65fd7884b2951053767c6b10f265ae77
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-13 21:33:12 +0800

    spring

--- src/main/java/spring/core/session04/sta/none/Man.java
+++ src/main/java/spring/core/session04/sta/none/Man.java
@@ -4,6 +4,8 @@ public class Man implements Person {
 
 	@Override
 	public void work() {
+		// 公用邏輯
+		System.out.println("戴口罩");
 		
 		// 業務邏輯
 		System.out.println("去 Google 上班");
--- src/main/java/spring/core/session04/sta/none/Woman.java
+++ src/main/java/spring/core/session04/sta/none/Woman.java
@@ -4,7 +4,9 @@ public class Woman implements Person {
 
 	@Override
 	public void work() {
-		
+		// 公用邏輯
+		System.out.println("戴口罩");
+				
 		// 業務邏輯
 		System.out.println("去 市場 買菜");
 		
