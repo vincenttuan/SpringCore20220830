ebd0879d7b97b89f87152733b275296abc6485d7
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-13 20:55:04 +0800

    spring

--- src/main/java/spring/core/session03/mvc/model/User.java
+++ src/main/java/spring/core/session03/mvc/model/User.java
@@ -6,29 +6,37 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component // 是一個可以被 Spring 來管理的物件
 @Scope("prototype")
+@PropertySource("classpath:user.properties")
 public class User { // 使用者資料模型
 	
-	@Value(value = "John") // 配置預設值 John
+	//@Value(value = "John") // 配置預設值 John
+	@Value("${user.username}")
 	private String username; // 姓名
 	
-	@Value(value = "18")
+	//@Value(value = "18")
+	@Value("${user.age}")
 	private Integer age; // 年齡
 	
-	@Value(value = "#{${nicknames: {'foo', 'bar'}}}") // 使用 Spring EL
+	//@Value(value = "#{${nicknames: {'foo', 'bar'}}}") // 使用 Spring EL
+	@Value("${user.nicknames}")
 	private String[] nicknames; // 暱稱
 	
-	@Value(value = "#{${subjects: {'Java', 'Python'}}}")
+	//@Value(value = "#{${subjects: {'Java', 'Python'}}}")
+	@Value("${user.subjects}")
 	private Set<String> subjects; // 專長科目
 	
-	@Value(value = "#{${scores: {100, 90}}}")
+	//@Value(value = "#{${scores: {100, 90}}}")
+	@Value("#{'${user.scores}'.split(',')}")
 	private List<Integer> scores; // 各科成績
 	
-	@Value(value = "#{${hobbies: {'h1': 'Program', 'h2': 'Game'}}}")
+	//@Value(value = "#{${hobbies: {'h1': 'Program', 'h2': 'Game'}}}")
+	@Value("${user.hobbies}")
 	private Map<String, String> hobbies; // 興趣
 	
 	public String getUsername() {
