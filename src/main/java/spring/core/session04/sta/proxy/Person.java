14182f1dd8431ed17210c306ba6262a6115822b7
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-13 21:34:21 +0800

    spring

---
+++ src/main/java/spring/core/session04/sta/proxy/Person.java
@@ -0,0 +1,5 @@
+package spring.core.session04.sta.proxy;
+
+public interface Person {
+	void work();
+}
---
+++ src/main/java/spring/core/session04/sta/proxy/Woman.java
@@ -0,0 +1,13 @@
+package spring.core.session04.sta.proxy;
+
+public class Woman implements Person {
+
+	@Override
+	public void work() {
+				
+		// 業務邏輯
+		System.out.println("去 市場 買菜");
+		
+	}
+	
+}
