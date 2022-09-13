4908085ff796f22242f44ad4f15da438c5f2d516
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-13 19:31:45 +0800

    spring

--- src/main/java/spring/core/session03/mvc/repository/UserDaoImpl.java
+++ src/main/java/spring/core/session03/mvc/repository/UserDaoImpl.java
@@ -3,8 +3,11 @@ package spring.core.session03.mvc.repository;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.stereotype.Repository;
+
 import spring.core.session03.mvc.model.User;
 
+@Repository
 public class UserDaoImpl implements UserDao {
 	
 	private List<User> users = new CopyOnWriteArrayList<>();
