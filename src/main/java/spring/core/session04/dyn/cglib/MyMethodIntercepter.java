2568cf2251db3738d562c5480c832aeaf2bcf081
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-15 21:01:51 +0800

    spring

---
+++ src/main/java/spring/core/session04/dyn/cglib/MyMethodIntercepter.java
@@ -0,0 +1,17 @@
+package spring.core.session04.dyn.cglib;
+
+import java.lang.reflect.Method;
+
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+// 方法攔截器 (給 cglib 使用)
+public class MyMethodIntercepter implements MethodInterceptor {
+
+	@Override
+	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+		System.out.println("請出示實聯 QRCode");
+		return proxy.invokeSuper(obj, args);
+	}
+	
+}
