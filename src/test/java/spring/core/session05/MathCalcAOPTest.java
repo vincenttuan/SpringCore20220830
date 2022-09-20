86baeac7c3863086afb08aa4da7132c07012d678
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-20 18:55:53 +0800

    spring

--- src/test/java/spring/core/session05/MathCalcAOPTest.java
+++ src/test/java/spring/core/session05/MathCalcAOPTest.java
@@ -1,7 +1,6 @@
 package spring.core.session05;
 
 import org.junit.Test;
-import org.springframework.cache.interceptor.CacheableOperation;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
