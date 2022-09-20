0ec4beb68257b05b64c3499aa1b6f9e586d4ed71
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-20 19:35:37 +0800

    spring

--- src/main/java/spring/core/session05/aop/MyLoggerAspect.java
+++ src/main/java/spring/core/session05/aop/MyLoggerAspect.java
@@ -5,11 +5,15 @@ import java.util.Arrays;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component // 可被 Spring 管理的元件
 @Aspect // 切面程式
 public class MyLoggerAspect {
+	// 建立一個切入點的方法
+	@Pointcut(value = "execution(* spring.core.session05.aop.MathCalcImpl.*(..))") 
+	public void pt() {}
 	
 	// 前置通知 Advice : 執行連接點之前所要執行的程式
 	//@Before(value = "execution(public Integer spring.core.session05.aop.MathCalcImpl.add(Integer, Integer))") // 切入點表達式 Spring EL : execution(..)
@@ -17,7 +21,8 @@ public class MyLoggerAspect {
 	//@Before(value = "execution(public Integer spring.core.session05.aop.MathCalcImpl.*(..))") // .. 表示方法中有 0~n 個參數
 	//@Before(value = "execution(* spring.core.session05.aop.MathCalcImpl.*(..))") // 第一個 * 表示任意修飾字與任意回傳型態
 	//@Before(value = "execution(* spring.core.session05.aop.*.*(..))") // *.* 表示 任意類.任意方法
-	@Before(value = "execution(* *(..))") // 全部通通攔截
+	//@Before(value = "execution(* *(..))") // 全部通通攔截
+	@Before(value = "pt()")
 	public void beforeAdvice(JoinPoint joinPoint) { // joinPoint 連接點
 		String methodName = joinPoint.getSignature().getName();  // 取得方法簽章的名字
 		Object[] args = joinPoint.getArgs(); // 方法參數
