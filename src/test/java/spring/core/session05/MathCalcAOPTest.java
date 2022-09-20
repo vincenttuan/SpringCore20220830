6ae8fa662f527bb10ae6f40f3135b2e349621a07
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-20 18:55:39 +0800

    spring

---
+++ src/test/java/spring/core/session05/MathCalcAOPTest.java
@@ -0,0 +1,22 @@
+package spring.core.session05;
+
+import org.junit.Test;
+import org.springframework.cache.interceptor.CacheableOperation;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session05.aop.MathCalc;
+import spring.core.session05.aop.MathCalcImpl;
+
+public class MathCalcAOPTest {
+	
+	@Test
+	public void test() {
+		// XML 配置
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("aop-config.xml");
+		MathCalc calc = ctx.getBean("mathCalcImpl", MathCalcImpl.class);
+		System.out.println(calc.add(20, 10));
+		System.out.println(calc.div(20, 10));
+	}
+	
+}
