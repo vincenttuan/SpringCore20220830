92251356de11c285414d09261d40ff08145eb7f7
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-22 19:15:16 +0800

    spring

--- src/main/java/spring/core/session05/AOPConfig.java
+++ src/main/java/spring/core/session05/AOPConfig.java
@@ -1,8 +1,11 @@
 package spring.core.session05;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import spring.core.session05.aop_lab.Introducer;
 
 @Configuration // 此為 Java 配置檔
 @ComponentScan // 啟用元件掃描, 預設會掃描 AOPConfig.class 所在目錄與其子目錄的 java 程式檔
@@ -10,4 +13,9 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 public class AOPConfig {
 	
+	@Bean
+	public Introducer getIntroducer() {
+		return new Introducer();
+				
+	}
 }
