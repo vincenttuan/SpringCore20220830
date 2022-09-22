f35774cc72ad95337967706d3b4dd45dd1d32511
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-22 21:12:48 +0800

    spring

--- target/classes/sample.sql
+++ target/classes/sample.sql
@@ -16,3 +16,35 @@ create table if not exists job (
     foreign key(eid) references emp(eid), -- 外鍵約束
     primary key(jid) -- 主鍵設定
 );
+
+-- 建立初始數據
+insert into emp(ename, age) values ('A01', 21);
+insert into emp(ename, age) values ('A02', 22);
+insert into emp(ename, age) values ('A03', 23);
+insert into emp(ename, age) values ('A04', 24);
+insert into emp(ename, age) values ('A05', 25);
+insert into emp(ename, age) values ('A06', 26);
+insert into emp(ename, age) values ('A07', 27);
+insert into emp(ename, age) values ('A08', 28);
+insert into emp(ename, age) values ('A09', 29);
+insert into emp(ename, age) values ('A10', 30);
+insert into emp(ename, age) values ('A11', 31);
+insert into emp(ename, age) values ('A12', 32);
+insert into emp(ename, age) values ('A13', 33);
+
+insert into job(jname, eid) values ('report', 1);
+insert into job(jname, eid) values ('coding', 3);
+insert into job(jname, eid) values ('jobA', 3);
+insert into job(jname, eid) values ('jobB', 4);
+insert into job(jname, eid) values ('jobC', 7);
+insert into job(jname, eid) values ('jobD', 12);
+insert into job(jname, eid) values ('jobE', 13);
+insert into job(jname, eid) values ('jobF', 1);
+insert into job(jname, eid) values ('jobG', 1);
+insert into job(jname, eid) values ('jobH', 3);
+insert into job(jname, eid) values ('jobI', 12);
+insert into job(jname, eid) values ('jobJ', 7);
+insert into job(jname, eid) values ('jobK', 4);
+insert into job(jname, eid) values ('jobL', 3);
+insert into job(jname) values ('jobM');
+insert into job(jname) values ('jobN');
