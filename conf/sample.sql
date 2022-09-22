c30b36bcad287b2e3cf742177986d23618cf6086
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-22 21:49:39 +0800

    spring

--- conf/sample.sql
+++ conf/sample.sql
@@ -48,3 +48,63 @@ insert into job(jname, eid) values ('jobK', 4);
 insert into job(jname, eid) values ('jobL', 3);
 insert into job(jname) values ('jobM');
 insert into job(jname) values ('jobN');
+
+
+use web;
+
+-- 1. 查詢所有員工資料
+-- select * from emp;
+-- 2. 查詢所有有工作的員工姓名, 工作名稱
+/*
+select e.ename, j.jname
+from emp e, job j
+where e.eid = j.eid
+*/
+-- 3. 請找出每一個員工有哪些工作 ? 
+--    透過 left join (向左關聯) 來實現
+--    因為是向左關聯, 所以會發現有些員工有工作, 有些員工沒有工作
+/*
+select e.ename, j.jname
+from emp e left join job j on e.eid = j.eid
+*/
+
+-- 4. 請找出每一個有工作的員工, 有員工的工作 ? 
+--    透過 inner join (交集) 來實現
+/*
+select e.ename, j.jname
+from emp e inner join job j on e.eid = j.eid 
+*/
+
+-- 5. 列出所有工作與對應的員工 ? 
+--    透過 right join (向右關聯) 來實現
+/*
+select e.ename, j.jname
+from emp e right join job j on e.eid = j.eid 
+*/
+
+-- 6. 利用 left join 找出沒工作的員工
+--    left join 配合 where 實現差集
+/*
+select e.ename, j.jname
+from emp e left join job j on e.eid = j.eid
+where j.jname is null
+*/
+
+-- 7. 利用 right join 找出沒員工的工作
+--    right join 配合 where 實現差集
+/*
+select e.ename, j.jname
+from emp e right join job j on e.eid = j.eid 
+where e.ename is null
+*/
+
+-- 8. 利用 left join 找出沒有工作的員工 與 right join 找出沒員工的工作
+--    最後透過 union all 來加在一起 
+select e.ename, j.jname
+from emp e left join job j on e.eid = j.eid
+where j.jname is null
+union all
+select e.ename, j.jname
+from emp e right join job j on e.eid = j.eid 
+where e.ename is null
+
