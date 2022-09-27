19439b6a8bb5238f097524253ab9f8a09d19b446
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-27 19:59:54 +0800

    spring

--- src/test/java/spring/core/session06/TestEmpDao1.java
+++ src/test/java/spring/core/session06/TestEmpDao1.java
@@ -2,11 +2,13 @@ package spring.core.session06;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.core.session06.entity.Emp;
 import spring.core.session06.template.EmpDao;
 
 public class TestEmpDao1 {
@@ -18,6 +20,21 @@ public class TestEmpDao1 {
 		EmpDao empDao = ctx.getBean("empDao", EmpDao.class);
 		List<Map<String, Object>> emps = empDao.queryAll();
 		System.out.println(emps);
+		// 利用 java stream 來分析資料
+		// 如何取得 eid = 2 的員工姓名 ?
+		Optional<Map<String, Object>> optEmp = emps.stream()
+				.filter(e -> (e.get("eid")+"").equals("2"))
+				.findFirst();
+		
+		if(optEmp.isPresent()) {
+			String ename = optEmp.get().get("ename") + "";
+			System.out.println(ename);
+		} else {
+			System.out.println("查無此人");
+		}
+		
+		
+		
 	}
 	
 }
