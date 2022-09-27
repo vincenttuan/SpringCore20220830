0b71b38e8d88800fad43e6593da174d7048e40cb
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-27 21:04:21 +0800

    spring

--- src/main/java/spring/core/conf/SpringJdbcConfig.java
+++ src/main/java/spring/core/conf/SpringJdbcConfig.java
@@ -8,8 +8,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 // Spring jdbc - Java 配置 (可以取代傳統的 jdbc-config.xml 配置檔)
 @Configuration // 告知 Spring 說此類是一個配置檔
@@ -43,6 +46,21 @@ public class SpringJdbcConfig {
 		}
 		
 		return ds;
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+	
+	@Bean 
+	public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(dataSource());
+	}
+	
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 	
 }
