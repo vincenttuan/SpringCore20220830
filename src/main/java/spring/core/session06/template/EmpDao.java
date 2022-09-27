a5a7d66cb7be2bce68fc876a6d217d0b15aa3e6a
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-27 19:41:35 +0800

    spring

---
+++ src/main/java/spring/core/session06/template/EmpDao.java
@@ -0,0 +1,29 @@
+package spring.core.session06.template;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+// DAO: Data Access Object
+@Repository
+public class EmpDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	// 多筆查詢: 全部查詢 I
+	public List<Map<String, Object>> queryAll() {
+		String sql = "select eid, ename, age, createtime from emp";
+		List<Map<String, Object>> emps = jdbcTemplate.queryForList(sql);
+		return emps;
+	}
+	
+	
+}
