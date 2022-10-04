365222d82e3db5bff3cc9fcb55b96472a4f8359a
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-04 19:14:53 +0800

    spring

---
+++ src/test/java/spring/core/session06/TestEmpDao11_Tx.java
@@ -0,0 +1,30 @@
+package spring.core.session06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.core.session06.entity.Emp;
+import spring.core.session06.template.EmpDao;
+
+public class TestEmpDao11_Tx {
+	
+	@Test
+	public void test() {
+		// 使用 xml 配置
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+		EmpDao empDao = ctx.getBean("empDao", EmpDao.class);
+		
+		// 新增 2 筆紀錄 (交易)
+		int[] rowscount = empDao.addTwoTx("B01", 18, "B02", 19);
+		if(rowscount != null) {
+			System.out.println(Arrays.toString(rowscount));
+		}
+		
+	}
+	
+}
