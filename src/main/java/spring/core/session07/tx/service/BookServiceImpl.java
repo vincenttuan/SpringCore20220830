42e32dac57421190bfae7fb612a01ffe4acc9da1
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-04 21:40:48 +0800

    spring

--- src/main/java/spring/core/session07/tx/service/BookServiceImpl.java
+++ src/main/java/spring/core/session07/tx/service/BookServiceImpl.java
@@ -2,6 +2,8 @@ package spring.core.session07.tx.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import spring.core.session07.tx.dao.BookDao;
 
@@ -9,7 +11,14 @@ import spring.core.session07.tx.dao.BookDao;
 public class BookServiceImpl implements BookService {
 	@Autowired
 	private BookDao bookDao;
-
+	
+	/*
+	 * Propagation.REQUIRED     : 如果有事務在運行, 當前方法就在該事物中運行, 
+	 *                            否則就啟動一個新的事物, 並在自己的事物運行
+	 * Propagation.REQUIRES_NEW : 當前方法必須啟動一個新的事物, 並在自己的事物運行
+	 *                            如果之前有事務正在運行, 就會將他掛起不用
+	 * */
+	@Transactional(propagation = Propagation.REQUIRED)
 	@Override
 	public void buyOne(Integer wid, Integer bid) {
 		// 1. 減去庫存
