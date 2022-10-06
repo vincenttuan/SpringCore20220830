99faba418d6982b3a124c2384c0a1d55340f763a
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-06 20:13:56 +0800

    spring

--- src/main/java/spring/core/session07/tx/controller/BookController.java
+++ src/main/java/spring/core/session07/tx/controller/BookController.java
@@ -3,6 +3,8 @@ package spring.core.session07.tx.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import spring.core.session07.tx.exception.InsufficientAmount;
+import spring.core.session07.tx.exception.InsufficientQuantity;
 import spring.core.session07.tx.service.BookService;
 import spring.core.session07.tx.service.ManyBookService;
 
@@ -15,12 +17,12 @@ public class BookController {
 	@Autowired
 	private ManyBookService manyBookService;
 	
-	public void buyOneBook(Integer wid, Integer bid) {
+	public void buyOneBook(Integer wid, Integer bid) throws InsufficientQuantity, InsufficientAmount {
 		bookService.buyOne(wid, bid);
 		System.out.println("Buy one book OK !");
 	}
 	
-	public void buyManyBooks(Integer wid, Integer... bids) {
+	public void buyManyBooks(Integer wid, Integer... bids) throws InsufficientQuantity, InsufficientAmount {
 		manyBookService.buyMany(wid, bids);
 		System.out.println("Buy many books OK !");
 	}
