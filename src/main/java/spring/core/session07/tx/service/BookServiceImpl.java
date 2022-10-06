a6e3636be7e3a094f0ec2a6fa756c8ec0333db5e
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-06 18:37:34 +0800

    spring

--- src/main/java/spring/core/session07/tx/service/BookServiceImpl.java
+++ src/main/java/spring/core/session07/tx/service/BookServiceImpl.java
@@ -17,6 +17,12 @@ public class BookServiceImpl implements BookService {
 	 *                            否則就啟動一個新的事物, 並在自己的事物運行
 	 * Propagation.REQUIRES_NEW : 當前方法必須啟動一個新的事物, 並在自己的事物運行
 	 *                            如果之前有事務正在運行, 就會將他掛起不用
+	 * Propagation.SUPPORTS     : 如果有事務在運行, 當前方法就在此事務中運行, 否則就不再事務中運行
+	 * Propagation.NOTSUPPORTED : 當前方法不應該在事務中運行, 如果有事務就會將它掛起不用
+	 * Propagation.MANDATORY    : 當前方法必須在運行在事務中, 若沒有就拋出異常
+	 * Propagation.NEVER        : 當前方法不應該運行在事務中, 若在事務中就拋出異常
+	 * Propagation.NESTED       : 如果有事務在運行, 當前方法必須嵌套在事務中, 否則就啟動一個新事務並在自己的事務中運行
+	
 	 * */
 	@Transactional(propagation = Propagation.REQUIRED)
 	@Override
